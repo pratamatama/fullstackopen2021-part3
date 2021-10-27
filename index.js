0e3c7ce55f428a060f6dbd3988caf4d72959039f
Donny Pratama <donnypratama1024@gmail.com>
2021-10-28 03:47:25 +0700

    chore: deploy heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
-var morgan = require('morgan')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors())
 app.use(express.json())
 
 let persons = [
@@ -96,7 +98,7 @@ app.get('/info', (request, response) => {
   `)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
